51d136c001b83c43b8458125b87aa8afc4e7c28d
Rob <robzlegz@gmail.com>
2023-06-18 22:40:24 +0300

    updt

--- mobile/src/modules/ScreenModule.tsx
+++ mobile/src/modules/ScreenModule.tsx
@@ -1,31 +1,24 @@
 import React from "react";
 import { basicScreen } from "../styles/screens";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Notification from "../components/notification/Notification";
 import { View, Platform } from "react-native";
 import {
   SafeAreaProvider,
   SafeAreaView,
   initialWindowMetrics,
 } from "react-native-safe-area-context";
-import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import { StatusBar } from "expo-status-bar";
 import { primary900 } from "../constants/colors";
-import ToastNotification from "../components/notification/ToastNotification";
 
 const ScreenModule = ({ ...props }) => {
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <SafeAreaView>
         {Platform.OS === "ios" ? (
-          <ExpoStatusBar style="light" />
+          <StatusBar style="light" />
         ) : (
-          <ExpoStatusBar style="light" backgroundColor={primary900} />
+          <StatusBar style="light" backgroundColor={primary900} />
         )}
-        <GestureHandlerRootView>
-          <View style={basicScreen}>{props.children}</View>
-          <Notification />
-          <ToastNotification />
-        </GestureHandlerRootView>
+        <View style={basicScreen}>{props.children}</View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- mobile/src/requests/categoryRequests.ts
+++ mobile/src/requests/categoryRequests.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { setCategories } from "../redux/slices/appSlice";
+import { setCategories, setCurrentCategory } from "../redux/slices/appSlice";
 import { setNotification } from "../redux/slices/notificationSlice";
 import { CATEGORY_BASE } from "./routes";
 import { Dispatch } from "@reduxjs/toolkit";
@@ -18,6 +18,37 @@ export const getCategories = async ({
     .get(CATEGORY_BASE, headrs)
     .then((res) => {
       dispatch(setCategories(res.data));
+    })
+    .catch((err) => {
+      if (!err.response) {
+        return console.log(err);
+      }
+
+      const message: string = err.response.data.err;
+      dispatch(
+        setNotification({
+          type: "error",
+          message: message,
+        })
+      );
+    });
+};
+
+export const getCategory = async ({
+  dispatch,
+  token,
+  id,
+}: {
+  dispatch: Dispatch;
+  token: string | null;
+  id: string;
+}) => {
+  const headrs = headers(token);
+
+  await axios
+    .get(`${CATEGORY_BASE}/${id}`, headrs)
+    .then((res) => {
+      dispatch(setCurrentCategory(res.data));
     })
     .catch((err) => {
       if (!err.response) {
