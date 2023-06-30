e62a0826ca54230d4b3a1a7d787031ea12af4b56
Rob <robzlegz@gmail.com>
2023-06-30 12:06:00 +0300

    all

---
+++ mobile/src/data/categories.ts
@@ -0,0 +1,50 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Confidence",
+    icon: "/assets/icons/confidence.png",
+    color: "#FF0043",
+    description:
+      "Full trust; belief in the powers, trustworthiness, or reliability of a person or thing: We have every confidence in their ability to succeed. belief in oneself and one's powers or abilities; self-confidence; self-reliance; assurance",
+  },
+  {
+    id: 2,
+    title: "Goal",
+    icon: "/assets/icons/goal.png",
+    color: "#FFC300",
+    description:
+      "Intention, intent, purpose, design, aim, end, object, objective, goal mean what one intends to accomplish or attain",
+  },
+  {
+    id: 3,
+    title: "Purpose",
+    icon: "/assets/icons/purpose.png",
+    color: "#D249D2",
+    description:
+      "The reason for which something is done or created or for which something exists.",
+  },
+  {
+    id: 4,
+    title: "Harmony",
+    icon: "/assets/icons/harmony.png",
+    color: "#D249D2",
+    description:
+      "The reason for which something is done or created or for which something exists.",
+  },
+  {
+    id: 5,
+    title: "Happiness",
+    icon: "/assets/icons/happiness.png",
+    color: "#D249D2",
+    description:
+      "The reason for which something is done or created or for which something exists.",
+  },
+  {
+    id: 6,
+    title: "Awareness",
+    icon: "/assets/icons/awareness.png",
+    color: "#D249D2",
+    description:
+      "The reason for which something is done or created or for which something exists.",
+  },
+];
