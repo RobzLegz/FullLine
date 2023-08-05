2f0aba4a73b84dbb0aefae0d7d7ce3c2de2f8048
Rob <robzlegz@gmail.com>
2023-08-05 12:08:59 +0300

    status

--- mobile/src/constants/colors.ts
+++ mobile/src/constants/colors.ts
@@ -4,10 +4,3 @@ export const white = "#FEFEFE";
 export const black = "#131212";
 export const gray = "#efeded";
 export const darkGray = "#8f8a8a";
-
-export const primary400 = "#DEE3EA";
-export const primary500 = "#B2BDCD";
-export const primary600 = "#5D7290";
-export const primary700 = "#242C37";
-export const primary800 = "#151A21";
-export const primary900 = "#0B0E11";
--- mobile/src/modules/ScreenModule.tsx
+++ mobile/src/modules/ScreenModule.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 import { basicScreen } from "../styles/screens";
-import { View, Platform } from "react-native";
+import { View } from "react-native";
 import {
   SafeAreaProvider,
   SafeAreaView,
   initialWindowMetrics,
 } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
-import { primary900 } from "../constants/colors";
 
 const ScreenModule = ({ ...props }) => {
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <SafeAreaView>
-        {Platform.OS === "ios" ? (
-          <StatusBar style="light" />
-        ) : (
-          <StatusBar style="light" backgroundColor={primary900} />
-        )}
+        <StatusBar style="dark" />
+
         <View style={basicScreen}>{props.children}</View>
       </SafeAreaView>
     </SafeAreaProvider>
