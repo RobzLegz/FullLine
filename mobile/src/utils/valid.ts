d73ab6f32ef4b228886de6d831aa2e8423db676a
Rob <robzlegz@gmail.com>
2023-08-05 12:27:06 +0300

    updt

--- mobile/src/utils/valid.ts
+++
@@ -1,6 +0,0 @@
-export const validEmail = (email: string) => {
-  const emailRe =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return email.match(emailRe);
-};
