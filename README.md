81bab299f6c6bc095385f24e752770822d5b3d8e
Rob <robzlegz@gmail.com>
2023-09-15 12:29:26 +0300

    replace

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-<a href="https://quickstack.lv"><p align="center">Developed by QuickStack.lv</p></a>
+# fulline
+fulline mobile app
 
-## Branches
-
-- main -> production
-
-## Code of Conduct
-
-Please read [CODE_OF_CONDUCT.md](https://github.com/RobzLegz/spotloc/blob/main/CODE_OF_CONDUCT.md) for details on our code of conduct.
-
-## License
-
-Please read [LICENSE.md](https://github.com/RobzLegz/spotloc/blob/main/LICENSE.md) for details on what you can use Spotloc source code for.
+<img 
+  src="https://github.com/spotorg/fulline-app/blob/main/readme/fulline-demo-1.jpg?raw=true"
+  width="300"
+/>
--- src/components/home/categories.ts
+++ src/components/home/categories.ts
@@ -18,12 +18,7 @@ export const categories = [
     title: "Confidence",
     icon: require("../../../assets/icons/confidence.png"),
     color: "#68C8F1",
-    images: [
-      { src: "", date: `${new Date("2023-08-16")}` },
-      { src: "", date: `${new Date("2023-08-20")}` },
-      { src: "", date: `${new Date("2023-08-21")}` },
-      { src: "", date: `${new Date("2023-08-25")}` },
-    ],
+    images: [],
     height: 0,
     description:
       "Share your moments of triumph and self-assurance with the Confidence category. This is the place to showcase your achievements, breakthroughs, and instances where you pushed past your comfort zone. Whether it's acing a presentation, overcoming a fear, or simply feeling empowered.",
@@ -33,20 +28,7 @@ export const categories = [
     title: "Goal",
     icon: require("../../../assets/icons/goal.png"),
     color: "#FFC956",
-    images: [
-      { src: "", date: `${new Date("2023-08-17")}` },
-      { src: "", date: `${new Date("2023-08-23")}` },
-      { src: "", date: `${new Date("2023-08-24")}` },
-      { src: "", date: `${new Date("2023-08-25")}` },
-      { src: "", date: `${new Date("2023-08-26")}` },
-      { src: "", date: `${new Date("2023-08-27")}` },
-      { src: "", date: `${new Date("2023-08-28")}` },
-      { src: "", date: `${new Date("2023-09-01")}` },
-      { src: "", date: `${new Date("2023-09-02")}` },
-      { src: "", date: `${new Date("2023-09-03")}` },
-      { src: "", date: `${new Date("2023-09-04")}` },
-      { src: "", date: `${new Date("2023-09-05")}` },
-    ],
+    images: [],
     height: 0,
     description:
       "Have a dream or a goal you're striving for? The Goal category is where you can document your journey towards success. Share your progress, milestones, and the steps you're taking to reach your aspirations. Celebrate both big wins and small victories.",
@@ -56,12 +38,7 @@ export const categories = [
     title: "Purpose",
     icon: require("../../../assets/icons/purpose.png"),
     color: "#6A6FF1",
-    images: [
-      { src: "", date: `${new Date("2023-08-15")}` },
-      { src: "", date: `${new Date("2023-08-22")}` },
-      { src: "", date: `${new Date("2023-08-26")}` },
-      { src: "", date: `${new Date("2023-08-29")}` },
-    ],
+    images: [],
     height: 0,
     description:
       "Finding your purpose in life can be a profound and fulfilling experience. In the Purpose category, share moments that align with your passions, values, and meaningful endeavors. This can include volunteering, engaging in a hobby you're passionate about, or contributing to a cause that holds a special place in your heart.",
@@ -71,7 +48,7 @@ export const categories = [
     title: "Harmony",
     icon: require("../../../assets/icons/harmony.png"),
     color: "#FE76A8",
-    images: [{ src: "", date: `${new Date("2023-09-07")}` }],
+    images: [],
     height: 0,
     description:
       "Life is full of moments that bring balance, tranquility, and a sense of serenity. Use the Harmony category to showcase images that capture the beauty of nature, peaceful moments, or instances where you've found harmony within yourself or with others.",
@@ -81,7 +58,7 @@ export const categories = [
     title: "Happiness",
     icon: require("../../../assets/icons/happiness.png"),
     color: "#C994DC",
-    images: [{ src: "", date: `${new Date("2023-09-09")}` }],
+    images: [],
     height: 0,
     description:
       "Spread joy and positivity with the Happiness category. Share pictures of heartwarming moments, laughter with friends, family gatherings, or any experience that brings a smile to your face.",
@@ -91,7 +68,7 @@ export const categories = [
     title: "Awareness",
     icon: require("../../../assets/icons/awareness.png"),
     color: "#FF814B",
-    images: [{ src: "", date: `${new Date("2023-09-12")}` }],
+    images: [],
     height: 0,
     description:
       "The Awareness category is all about being present and mindful. Share images that remind you to be aware of your surroundings, emotions, and the world. This could include pictures from mindfulness practices, meditation, or moments of self-reflection that lead to a deeper understanding of yourself and the world around you.",
