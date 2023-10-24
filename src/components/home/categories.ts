96e66747989af2d30df2c5221c630376f1ff7169
RobzLegz <robzlegz@gmail.com>
2023-10-24 18:45:57 +0300

    txt updates

--- src/components/home/categories.ts
+++ src/components/home/categories.ts
@@ -21,7 +21,7 @@ export const categories = [
     images: [],
     height: 0,
     description:
-      "Reflect on whether you feel confident in various aspects of your life. Have there been situations where your confidence was lacking? If so, what contributed to this lack of confidence? To improve in this area, consider setting achievable goals, seeking mentorship or support, and working on building self-esteem.",
+      "Share your moments of triumph and self-assurance with the Confidence category. This is the place to showcase your achievements, breakthroughs, and instances where you pushed past your comfort zone. Whether it's acing a presentation, overcoming a fear, or simply feeling empowered.",
   },
   {
     id: 2,
@@ -31,7 +31,7 @@ export const categories = [
     images: [],
     height: 0,
     description:
-      "Reflect on the importance of setting and achieving goals in your life. How would reaching more of your goals positively impact you? To improve in this area, set specific, measurable, and time-bound goals. Break them down into smaller steps and create an action plan to work towards them.",
+      "Have a dream or a goal you're striving for? The Goal category is where you can document your journey towards success. Share your progress, milestones, and the steps you're taking to reach your aspirations. Celebrate both big wins and small victories.",
   },
   {
     id: 3,
@@ -41,7 +41,7 @@ export const categories = [
     images: [],
     height: 0,
     description:
-      "Consider whether you are still aligned with your life's purpose and if you have a clear vision of what that purpose is. If you feel disconnected from your purpose, take time to reevaluate your goals and values. Seek guidance from mentors or explore new interests that align with your sense of purpose.",
+      "Finding your purpose in life can be a profound and fulfilling experience. In the Purpose category, share moments that align with your passions, values, and meaningful endeavors. This can include volunteering, engaging in a hobby you're passionate about, or contributing to a cause that holds a special place in your heart.",
   },
   {
     id: 4,
@@ -51,7 +51,7 @@ export const categories = [
     images: [],
     height: 0,
     description:
-      "Assess whether there is a balance and harmony in your life, or if one area dominates your time and energy. To restore harmony, consider time management techniques, setting boundaries, and delegating tasks if necessary. Prioritize a well-rounded life where various aspects receive attention.",
+      "Life is full of moments that bring balance, tranquility, and a sense of serenity. Use the Harmony category to showcase images that capture the beauty of nature, peaceful moments, or instances where you've found harmony within yourself or with others.",
   },
   {
     id: 5,
@@ -61,7 +61,7 @@ export const categories = [
     images: [],
     height: 0,
     description:
-      "Examine whether you are dedicating enough time to self-care and activities that bring you happiness. To boost your happiness, prioritize self-care, engage in hobbies and activities you enjoy, and spend time with loved ones. Remember that taking care of your well-being is essential.",
+      "Spread joy and positivity with the Happiness category. Share pictures of heartwarming moments, laughter with friends, family gatherings, or any experience that brings a smile to your face.",
   },
   {
     id: 6,
@@ -71,6 +71,6 @@ export const categories = [
     images: [],
     height: 0,
     description:
-      "Think about your level of awareness regarding your surroundings and inner self. Has daily life become so consuming that you've lost touch with what's happening around you or within you? To improve awareness, try mindfulness practices, meditation, or simply setting aside time for self-reflection each day.",
+      "The Awareness category is all about being present and mindful. Share images that remind you to be aware of your surroundings, emotions, and the world. This could include pictures from mindfulness practices, meditation, or moments of self-reflection that lead to a deeper understanding of yourself and the world around you.",
   },
 ];
